f6fd269453f6b538cadfa9b42b4156361557f347
Sabane Deepak <deepak.sabane@peopletech.com>
2024-06-13 04:51:50 -0700

    this is the latest commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "ec2_instance" {
     source = "./modules/ec2_instance"
     ami = var.ami
-    instance_type = lookup(instance_type, terraform.workspace, t3.xlarge) #it will see the instance type and it will see the workspace in which it is present  it wil create the resource there no need to specify the tfvars file while executing the command  
+    instance_type = lookup(var.instance_type, terraform.workspace, t3.xlarge) #it will see the instance type and it will see the workspace in which it is present  it wil create the resource there no need to specify the tfvars file while executing the command  
   
 }
 
