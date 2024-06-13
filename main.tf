8dd26e16fdac75b765bb71b36fc88b5e54ef8bd7
Sabane Deepak <deepak.sabane@peopletech.com>
2024-06-13 04:25:51 -0700

    this is the commit

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
-provider "aws" {
-    region = "ap-south-2"
+module "ec2_instance" {
+    source = "./modules/ec2_instance"
+    ami = var.ami
+    instance_type = lookup(instance_type, terraform.workspace, t3.xlarge) #it will see the instance type and it will see the workspace in which it is present  it wil create the resource there no need to specify the tfvars file while executing the command  
   
 }
 
@@ -10,12 +12,11 @@ variable "ami" {
 
 variable "instance_type" {
     description = "value"
-  
-}
-
-module "ec2_instance" {
-    source = "./modules/ec2_instance"
-    ami = var.ami
-    instance_type = var.instance_type
+    type = map(string) # in which environment the ec2 instance of which type should it create 
+    default = {
+      "dev" = "t2.micro"
+      "stage" = "t3.medium"
+      "prod" = "t3.small"
+    }
   
 }
